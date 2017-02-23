4479349b84ddd44886f5df6f34435f887a26e249
NikitaSadinidhiVijayan <sv.nikita91@yahoo.com>
2017-02-22 20:34:40 -0800

    added hijack
    hijack added

---
+++ ottergram/hijack.js
@@ -0,0 +1,9 @@
+var arraylinks = [].slice.call(document.querySelectorAll('a'));
+function disable(arraylinks){
+	'use strict';
+	arraylinks.addEventListener("click", function(event){
+    event.preventDefault();
+});
+
+arraylinks.forEach(disable)
+
